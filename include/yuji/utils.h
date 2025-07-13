9726d36bada249e349f54656f2793fec85949694
0xM4LL0C <0xm4ll0c@proton.me>
2025-07-13 20:33:15 +0400

    improved LOG macro

--- include/yuji/utils.h
+++ include/yuji/utils.h
@@ -1,16 +1,16 @@
 #pragma once
 
-
 #define ANSI_RED "\x1b[31m"
 #define ANSI_RESET "\x1b[0m"
 #define ANSI_BLUE "\x1b[34m"
+#define ANSI_PURPLE "\x1b[35m"
 
 #ifndef NDEBUG
-#define LOG(...) \
-  printf(ANSI_BLUE "LOG: " ANSI_RESET __VA_ARGS__); \
-  printf("\n");
+#define LOG(fmt, ...) \
+    printf(ANSI_BLUE "LOG: " ANSI_PURPLE "%s:%d:%s" ANSI_RESET " | " fmt "\n", \
+      __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #else
-#define LOG(...)
+#define LOG(fmt, ...)
 #endif
 
 void panic(const char* message, ...) __attribute__((noreturn)) ;
