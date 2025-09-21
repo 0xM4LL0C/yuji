5e7f81fcd420e2ed7ae7245e640b7e21a692e218
0xM4LL0C <0xm4ll0c@proton.me>
2025-09-21 18:13:26 +0400

    refactor

--- include/yuji/core/scope.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "yuji/core/interpreter.h"
-#include "yuji/core/types/map.h"
-
-
-YujiScope* yuji_scope_init(YujiScope* parent);
-void yuji_scope_free(YujiScope* scope);
--- src/core/scope.c
+++
@@ -1,3 +0,0 @@
-#include "yuji/core/scope.h"
-#include "yuji/core/interpreter.h"
-#include "yuji/core/memory.h"
