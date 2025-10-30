bb29fe552c4ed20293c3732151daae48831f5025
0xM4LL0C <0xm4ll0c@proton.me>
2025-10-31 00:00:05 +0400

    refactor

--- src/core/interpreter.c
+++ src/core/interpreter.c
@@ -381,7 +381,7 @@ YujiValue* yuji_interpreter_eval(YujiInterpreter* interpreter, YujiASTNode* node
       if (fn->type == VT_CFUNCTION) {
         if (fn->value.cfunction->argc != YUJI_FN_INF_ARGUMENT
             && fn->value.cfunction->argc != call->args->size) {
-          yuji_panic("function '%s' expects %d, got %d", call->name, fn->value.cfunction->argc,
+          yuji_panic("function '%s' expects %ld, got %ld", call->name, fn->value.cfunction->argc,
                      call->args->size);
         }
 
