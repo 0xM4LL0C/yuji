ada5fffba2e21fe4e85f1fba2c811987052ce34e
0xM4LL0C <0xm4ll0c@proton.me>
2025-11-03 23:16:17 +0400

    removed 'malloc' attribute arguments in memory.h (#39)

--- include/yuji/core/memory.h
+++ include/yuji/core/memory.h
@@ -7,5 +7,5 @@ NO_RETURN __attribute__((format(printf, 1, 2))) void yuji_panic(const char* fmt,
 void yuji_check_memory(void* ptr);
 
 void yuji_free(void* ptr);
-__attribute__((malloc(yuji_free, 1))) void* yuji_malloc(size_t size);
-__attribute__((malloc(yuji_free, 1))) void* yuji_realloc(void* ptr, size_t size);
+__attribute__((malloc)) void* yuji_malloc(size_t size);
+__attribute__((malloc)) void* yuji_realloc(void* ptr, size_t size);
